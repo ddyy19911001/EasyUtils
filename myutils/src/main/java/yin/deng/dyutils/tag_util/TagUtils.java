da9971f5dab19f94f457f49e1532a40f4ffeccb4
邓银 <584120792@qq.com>
2019-04-16 18:04:56 +0800

    增加标签布局

---
+++ myutils/src/main/java/yin/deng/dyutils/tag_util/TagUtils.java
@@ -0,0 +1,92 @@
+package yin.deng.dyutils.tag_util;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
+import com.zhy.view.flowlayout.TagFlowLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class TagUtils {
+   static TagUtils tagUtils;
+   List<String>datas=new ArrayList<>();
+    private TagUtils() {
+    }
+
+    public static TagUtils getInstance(){
+        if(tagUtils==null){
+            tagUtils=new TagUtils();
+        }
+        return tagUtils;
+    }
+
+    /**
+     * 是否在布局中使用默认选中效果
+     * @param tagFlowLayout
+     * @param isOpen
+     */
+    public void setAutoSelectEffet(TagFlowLayout tagFlowLayout,boolean isOpen){
+        tagFlowLayout.setClickable(isOpen);
+    }
+
+    /**
+     *
+     * @param tagFlowLayout
+     * @param tabRootView tab根view
+     * @param tv   显示标签的textView
+     * @param datas  显示的标签列表
+     */
+    public void initTagLayout(final TagFlowLayout tagFlowLayout, final View tabRootView, final TextView tv, List<String>datas){
+        this.datas.clear();
+        this.datas.addAll(datas);
+        tagFlowLayout.setAdapter(new TagAdapter<String>(datas){
+            @Override
+            public View getView(FlowLayout parent, int position, String s) {
+                tv.setText(s);
+                if(tabRootView==null){
+                    return tv;
+                }else {
+                    return tabRootView;
+                }
+            }
+        });
+    }
+
+    public void setOnTagClickListener(TagFlowLayout tagFlowLayout,TagFlowLayout.OnTagClickListener listener){
+        tagFlowLayout.setOnTagClickListener(listener);
+    }
+
+    public void setOnSelectedListener(TagFlowLayout tagFlowLayout,TagFlowLayout.OnSelectListener listener){
+        tagFlowLayout.setOnSelectListener(listener);
+    }
+
+    /**
+     * 获取已经选择的tag
+     * @param tagFlowLayout
+     * @return
+     */
+    public List<String> getSelectedList(TagFlowLayout tagFlowLayout){
+        Set<Integer> selecteds = tagFlowLayout.getSelectedList();
+        List<String> selectedsStr=new ArrayList<>();
+        for(int i:selecteds){
+            selectedsStr.add(datas.get(i));
+        }
+        return selectedsStr;
+    }
+
+
+    /**
+     * 设置先选中部分tag
+     * @param tagFlowLayout
+     * @param i
+     */
+    public void setSeletedSomeListTag(TagFlowLayout tagFlowLayout,int... i){
+        TagAdapter mAdapter = tagFlowLayout.getAdapter();
+        //预先设置选中
+        mAdapter.setSelectedList(i);
+    }
+}
