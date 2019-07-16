00ababd82e445f0d5d9d7bed2559fcee2d93c847
liuye <liuye@51web.com>
2019-07-16 17:24:46 +0800

    修复jsoup不再使用jar包

--- app/src/main/java/yin/deng/easyutils/MainActivity.java
+++ app/src/main/java/yin/deng/easyutils/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends SuperBaseActivity {
         initView();
         String []permisions={Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE};
         requestRunTimePermission(permisions,this);
-        JsoupUtils.getHtmlDoctmentNoParams(this, "https://download.csdn.net/download/czxttkl/5411175", new JsoupUtils.OnDataGetListener() {
+        JsoupUtils.getHtmlDoctmentNoParams(this, "https://porn.jmmdh.xyz/search/" + "女" + "/" + 0 + "/", new JsoupUtils.OnDataGetListener() {
             @Override
             public void onGetData(Document data) {
                 showTs("获取到数据了");
@@ -50,12 +50,6 @@ public class MainActivity extends SuperBaseActivity {
     @Override
     public void onGranted() {
         LogUtils.i("授权成功");
-        tvContent.setOnClickListener(new NoDoubleClickListener() {
-            @Override
-            protected void onNoDoubleClick(View v) {
-                getHttpUtils().download("http://p.gdown.baidu.com/50b689a32183224df9c41613d13be2c7cd0bb79bfa82410927aef40074893fb4183e5d14eb4135930e6441693b5502c7ce6774473a9fcfe3b9f14925aa7018a8ddc9c9fb09708a598d09d4cfadfcf9ac9388039d0d8387c752651f73c5d0e634e3aca94aeb14952cb5831b0a554f2803266ee20c0802107e2950af6e47c5119a0cc9f936c3a6e00a71f0e667f4ebb5dc92b48beff16d6c0b75302946e5741763be32b4a1df43ce9ee5a320716d893ef1","qq.apk");
-            }
-        });
     }
 
 
